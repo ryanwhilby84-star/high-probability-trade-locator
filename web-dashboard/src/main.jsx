c06fe4cbb86c0d1cd21b7a2f68c64b0392fcab3a
ryanwhilby84-star <ryanwhilby84@gmail.com>
2026-08-26 12:38:44 +0100

    remove obsolete workstation override layer

--- web-dashboard/src/main.jsx
+++ web-dashboard/src/main.jsx
@@ -7,6 +7,5 @@ import './theme-institutional.css'
 import './cot-heatmap.css'
 import './workstation/styles/instrumentWorkstation.css'
 import './workstation/workstationModern.css'
-import './workstation/workstationLightFinal.css'
 
 createRoot(document.getElementById('root')).render(<App />)
